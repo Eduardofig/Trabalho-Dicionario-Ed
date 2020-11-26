acfd5baf3ed24269a57643613a6a4a8a4fe5ff8e
Eduardo Figueiredo Freire Amdradee <duzinho039@pop-os.localdomain>
2020-11-26 17:40:28 -0300

    Operacao 1: sem testar

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include "AVL.h"
+
+struct _avl
+{
+    int id;
+};
+
+int main(int argc, char *argv[])
+{
+    int seletor, n_palavras, n_dicionarios = 0;
+    ITEM palavra;
+    Boolean flag;
+    AVL *dicionarios[3];
+
+    while (flag == TRUE) {
+        scanf("%d", &seletor);
+        switch (seletor) {
+            case 1:
+                if (n_dicionarios < 4) {
+                    dicionarios[n_dicionarios] = avl_criar();
+                    dicionarios[n_dicionarios]->id = n_dicionarios + 1;
+                    scanf("%d", &n_palavras);
+                    for (int i = 0; i < n_palavras; ++i) {
+                        scanf("%s", palavra);
+                        printf("%d\n", avl_inserir(dicionarios[n_dicionarios], palavra));
+                    }
+                    printf("DICIONARIO %d CRIADO\n", dicionarios[n_dicionarios]->id);
+                    ++n_dicionarios;
+                } else {
+                    printf("IMPOSSIVEL CRIAR\n");
+                }
+                break;
+            default:
+                flag = FALSE;
+                break;
+        }
+    }
+
+    return 0;
+}
