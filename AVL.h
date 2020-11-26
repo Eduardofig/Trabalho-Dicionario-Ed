845d119ba536de2d47e7b5c7ef300f1513f7205f
Rafaela Bull <37142843+rafabull@users.noreply.github.com>
2020-11-26 15:52:03 -0300

    tab fix

--- AVL.h
+++ AVL.h
@@ -5,8 +5,8 @@
  */
 
 
-#ifndef AVL
-#define AVL
+#ifndef AVL_HEADER
+#define AVL_HEADER
 
 #define Boolean int
 #define TRUE 1
@@ -18,11 +18,11 @@ typedef struct _avl AVL;
 
 typedef char ITEM[MAX] ;
 
-AVL	*avl_criar(void);	
-void avl_apagar(AVL	**arvore);	
-Boolean avl_inserir(AVL	*T,	ITEM item);	
-Boolean avl_remover(AVL	*T,	int	chave);	
-ITEM* avl_buscar(AVL	*T,	int	chave);	
-int avl_altura(AVL*T);	
+AVL* avl_criar();
+void avl_apagar(AVL **arvore);
+Boolean avl_inserir(AVL *T, ITEM item);
+Boolean avl_remover(AVL *T, int chave);
+ITEM* avl_buscar(AVL *T, int chave);
+int avl_altura(AVL*T);
 
-#endif /* ifndef AVL */
+#endif /* ifndef AVL_HEADER */
