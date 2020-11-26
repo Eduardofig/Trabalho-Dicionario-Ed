f67f8ac56d3fa0e1a5ce16af657554cfa3fc21e3
Eduardofig <duzinho039@pop-os.localdomain>
2020-11-26 19:12:42 -0300

    Operacao 1: Melhoria na logica.

--- main.c
+++ main.c
@@ -9,28 +9,33 @@ struct _avl
 
 int main(int argc, char *argv[])
 {
-    int seletor, n_palavras, n_dicionarios = 0;
+    int seletor, n_palavras, n_dicionarios = 0, i, j;
     ITEM palavra;
     Boolean flag;
     AVL *dicionarios[3];
 
-    while (flag == TRUE) {
+    for (i = 0; i < 3; ++i) {
+        dicionarios[i] = NULL;
+    }
+
+    while (flag) {
         scanf("%d", &seletor);
         switch (seletor) {
             case 1:
-                if (n_dicionarios < 3) {
-                    dicionarios[n_dicionarios] = avl_criar();
-                    dicionarios[n_dicionarios]->id = n_dicionarios + 1;
-                    scanf("%d", &n_palavras);
-                    for (int i = 0; i < n_palavras; ++i) {
-                        scanf("%s", palavra);
-                        printf("%d\n", avl_inserir(dicionarios[n_dicionarios], palavra));
+                for (i = 0; i < 3; ++i) {
+                    if (dicionarios[i] == NULL) {
+                        dicionarios[i] = avl_criar();
+                        dicionarios[i]->id = i + 1;
+                        scanf("%d", &n_palavras);
+                        for (j = 0; j < n_palavras; ++j) {
+                            scanf("%s", palavra);
+                            printf("%d\n", avl_inserir(dicionarios[i], palavra));
+                        }
+                        printf("DICIONARIO %d CRIADO\n", dicionarios[i]->id);
+                        break;
                     }
-                    printf("DICIONARIO %d CRIADO\n", dicionarios[n_dicionarios]->id);
-                    ++n_dicionarios;
-                } else {
-                    printf("IMPOSSIVEL CRIAR\n");
                 }
+                printf("IMPOSSIVEL CRIAR\n");
                 break;
             default:
                 flag = FALSE;
