9cc5d6da8fd6211eceb51a6bdefc32f7f206db14
Eduardofig <duzinho039@pop-os.localdomain>
2020-11-27 11:36:40 -0300

    👌 MELHORIA: Criacao de struct _dicionario para melhor administrar os IDs e os diferentes dicionarios

--- main.c
+++ main.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "AVL.h"
 
-struct _avl
+typedef struct _dicionario
 {
     int id;
-};
+    AVL* avl;
+}DICIONARIO;
 
 int main(int argc, char *argv[])
 {
     int seletor, n_palavras, n_dicionarios = 0, i, j;
     ITEM palavra;
     Boolean flag;
-    AVL *dicionarios[3];
+    DICIONARIO *dicionarios[3];
 
     for (i = 0; i < 3; ++i) {
         dicionarios[i] = NULL;
@@ -24,12 +26,13 @@ int main(int argc, char *argv[])
             case 1:
                 for (i = 0; i < 3; ++i) {
                     if (dicionarios[i] == NULL) {
-                        dicionarios[i] = avl_criar();
+                        dicionarios[i] = (DICIONARIO*) malloc(sizeof(DICIONARIO));
+                        dicionarios[i]->avl = avl_criar();
                         dicionarios[i]->id = i + 1;
                         scanf("%d", &n_palavras);
                         for (j = 0; j < n_palavras; ++j) {
                             scanf("%s", palavra);
-                            printf("%d\n", avl_inserir(dicionarios[i], palavra));
+                            printf("%d\n", avl_inserir(dicionarios[i]->avl, palavra));
                         }
                         printf("DICIONARIO %d CRIADO\n", dicionarios[i]->id);
                         break;
