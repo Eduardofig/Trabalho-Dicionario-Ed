2563f2e45ffc969481cffe54c1b4117bebf3af84
Eduardofig <duzinho039@pop-os.localdomain>
2020-12-03 19:11:57 -0300

    :goal_net: CATCH ERROR: Mudanca no dicionario inexistente
    Mudanca para ele continuar aceitando o input das palavras mesmo quando o dicionario nao existe ou quando nao e possivel criar o dicionario(no caso da operacao 1)

--- main.c
+++ main.c
@@ -21,11 +21,10 @@ int comparador_recorrentes(const void *a, const void *b)
 int main(int argc, char *argv[])
 {
     int seletor, i, seletor_atualizacao, indice_dicionario,n_palavras_recorrentes, tamanho_arr_recorrentes;
-    ITEM chave, char_flag, palavra, *recorrencia;
+    ITEM char_flag, palavra, *recorrencia;
     Boolean flag = TRUE, funcionou;
     DICIONARIO *dicionarios[3];
-    ITEMREC recorrentes, *recorrentes_aux;
-    ITEMREC *array_recorrentes;
+    ITEMREC *recorrentes_aux, *array_recorrentes;
     AVLREC *AVLrec;
 
     for (i = 0; i < 3; ++i) {
@@ -55,7 +54,16 @@ int main(int argc, char *argv[])
                         break;
                     }
                 }
-                funcionou? printf("DICIONARIO %d CRIADO\n", dicionarios[i]->id): printf("IMPOSSIVEL CRIAR\n");
+                /*Caso nao seja possivel criar o dicionario*/
+                if (!funcionou) {
+                    scanf("%s", palavra);
+                    while (strcmp(palavra, "#") != 0) {
+                        scanf("%s", palavra);
+                    }
+                    printf("IMPOSSIVEL CRIAR\n");
+                    break;
+                }
+                printf("DICIONARIO %d CRIADO\n", dicionarios[i]->id);
                 break;
             case 2:
                 scanf("%d", &indice_dicionario);
@@ -67,16 +75,22 @@ int main(int argc, char *argv[])
                         seletor_atualizacao = atoi(char_flag);
                         if (seletor_atualizacao == 0) {
                             /*Remocao de palavra do dicionario*/
-                            funcionou = avl_remover(dicionarios[indice_dicionario]->avl, chave);
-                            printf(funcionou? "%s EXCLUIDA DE %d\n" : "%s INEXISTENTE EM %d", chave, dicionarios[indice_dicionario]->id);
+                            funcionou = avl_remover(dicionarios[indice_dicionario]->avl, palavra);
+                            printf(funcionou? "%s EXCLUIDA DE %d\n" : "%s INEXISTENTE EM %d", palavra, dicionarios[indice_dicionario]->id);
                         } else {
                             /*Insercao de palavra no dicionario*/
-                            funcionou = avl_inserir(dicionarios[indice_dicionario]->avl, chave);
-                            printf(funcionou? "%s INSERIDA EM %d\n" : "%s JA EXISTE EM %d", chave, dicionarios[indice_dicionario]->id);
+                            funcionou = avl_inserir(dicionarios[indice_dicionario]->avl, palavra);
+                            printf(funcionou? "%s INSERIDA EM %d\n" : "%s JA EXISTE EM %d", palavra, dicionarios[indice_dicionario]->id);
                         }
                         scanf("%s", char_flag);
                     }
                     break;
+                }
+                /*Caso o dicionario nao exista*/
+                scanf("%s", char_flag);
+                while (strcmp(char_flag, "#") != 0) {
+                    scanf("%s", palavra);
+                    scanf("%s", char_flag);
                 }
                 printf("DICIONARIO %d INEXISTENTE\n", indice_dicionario + 1);
                 break;
@@ -98,22 +112,22 @@ int main(int argc, char *argv[])
                 --indice_dicionario;
                 if (dicionarios[indice_dicionario] != NULL) {
                     AVLrec = avlrec_criar();
-                    scanf("%s", recorrentes.palavra);
-                    while (strcmp(recorrentes.palavra, "#") != 0) {;
+                    scanf("%s", palavra);
+                    while (strcmp(palavra, "#") != 0) {
                         /*Busca da palavra no dicionario, caso ache entra no if*/
-                        recorrencia = avl_buscar(dicionarios[indice_dicionario]->avl, recorrentes.palavra);
+                        recorrencia = avl_buscar(dicionarios[indice_dicionario]->avl, palavra);
                         if (strcmp(*recorrencia, ERRO_STRING) != 0) {
-                            recorrentes_aux = avlrec_buscar(AVLrec, recorrentes.palavra);
+                            recorrentes_aux = avlrec_buscar(AVLrec, palavra);
                             if (strcmp(recorrentes_aux->palavra, ERRO_STRING) != 0) {
                                 ++recorrentes_aux->recorrencia;
                             } else {
-                                avlrec_inserir(AVLrec, recorrentes.palavra);
+                                avlrec_inserir(AVLrec, palavra);
                             }
                         } else {
                             /*Caso nao ache, imprime a palavra*/
-                            printf("%s\n", recorrentes.palavra);
+                            printf("%s\n", palavra);
                         }
-                        scanf("%s", recorrentes.palavra);
+                        scanf("%s", palavra);
                     }
                     array_recorrentes = avlrec_toarray(AVLrec, &tamanho_arr_recorrentes);
                     qsort(array_recorrentes, tamanho_arr_recorrentes, sizeof(ITEMREC), comparador_recorrentes);
@@ -124,6 +138,11 @@ int main(int argc, char *argv[])
                     avlrec_apagar(AVLrec);
                     AVLrec = NULL;
                     break;
+                }
+                /*Caso o dicionario nao exista*/
+                scanf("%s", palavra);
+                while (strcmp(palavra, "#")) {
+                    scanf("%s", palavra);
                 }
                 printf("DICIONARIO %d INEXISTENTE\n", indice_dicionario + 1);
                 break;
